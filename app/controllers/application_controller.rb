988591de8d613b6ad745ebd7dafcaf5618284179
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2022-12-09 10:20:18 -0500

    commit tree d4661b59cf6af167548a0ecda26444b486801f29

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < Sinatra::Base
 
+  
+  set default_content_type: "application/json"
   # add routes
+
+  get "/bakeries" do 
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 
 end
